7cca109e68b68d619b8d294e0a6ef7164ece477c
Sadie Thongsavanh <sadiethongsavanh@Sadies-Air.lan>
2024-04-09 14:26:15 -0500

    added route for returning all dogs, in progress

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -9,6 +9,10 @@ const router = require("express").Router()
 router.post("/newdog", withAuth, async(req,res) => {
     const newDog = Dog.create({...req.body, user_id: req.session.user_id})
 });
-
+//attempt to retrieve all dogs from database
+router.get('/', (req, res) => {
+    console.log("return all dogs")
+   
+});
 module.exports = router;
 //TO DO: GET Route for all dogs, with auth
