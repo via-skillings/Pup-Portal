215ae42fa73fff656839ae9ec4d58b5d952241b6
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-04-09 18:48:12 -0500

    removed associations for deleted models

--- models/index.js
+++ models/index.js
@@ -1,19 +1,20 @@
 const User = require('./user');
 const Dog = require('./dog');
-const Traits = require('./traits');
-const Special = require('./special');
+// const Traits = require('./traits');
+// const Special = require('./special');
 
+//commented out user traits an special needs model associations since those tables/features are removed from this release of the app -VG
 
-Dog.hasMany(Traits, {
-    foreignKey: 'dog_id'
-  });
-  Traits.belongsTo(Dog);
+// Dog.hasMany(Traits, {
+//     foreignKey: 'dog_id'
+//   });
+//   Traits.belongsTo(Dog);
 
-  Dog.hasMany(Special, {
-    foreignKey: 'dog_id'
-  });
-  Special.belongsTo(Dog);
+//   Dog.hasMany(Special, {
+//     foreignKey: 'dog_id'
+//   });
+//   Special.belongsTo(Dog);
 
-module.exports = { User, Dog, Traits, Special };
+module.exports = { User, Dog };
 
 
